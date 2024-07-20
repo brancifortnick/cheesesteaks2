ae891e1b4757f72a6af859c30f40eca381983a3e
nicholas <brancifortnick@yahoo.com>
2024-07-20 15:45:35 -0400

    adding imaging style=====> SAFE AND HEALTHY PUSH ======> 07.20

--- react-app/src/components/AllImagesRefactor.js
+++ react-app/src/components/AllImagesRefactor.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import DeleteLocationsImages from "./DeleteLocationsImages";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 import CardActions from "@mui/material/CardActions";
 import AddComments from "./AddComments";
 import Accordian from "./Accordian/Accordian";
@@ -15,7 +15,10 @@ import EditComment from "./EditComment";
 import VoteCounter from "./VoteCounter";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
-import './AllImagesRefactor.css'
+import "./AllImagesRefactor.css";
+
+
+
 function AllImagesRefactor({ images }) {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.session.user);
@@ -36,7 +39,7 @@ function AllImagesRefactor({ images }) {
       });
     }
   };
-  //mui styling funcs for images
+  
 
   //returning multiple accordians
   const buildTemplate = () => {
@@ -44,20 +47,8 @@ function AllImagesRefactor({ images }) {
       return images.map((image) => {
         return image !== null && location.id === image.location_id ? (
           <div className="images-container">
-               <div>{image.title}</div>
-           
-             
-                <img
-                  src={image.image}
-                  className="image-card"
-                  alt="loading..."
-                />
-
-         
-               
-            
-           
-
+            <div>{image.title}</div>
+            <img src={image.image} className="image-card" alt="loading..." />
             <span>
               <AddComments imageId={image.id} locationId={location.id} />
             </span>
@@ -67,20 +58,17 @@ function AllImagesRefactor({ images }) {
                 children={buildCommentTemplate(image)}
               />
             </span>
-
             <CardContent>
               <CardActions>
                 <DeleteLocationsImages imageId={image?.id} />
                 {/* {user.id === Number(location.user_id) ? (  ) : null} */}
               </CardActions>
             </CardContent>
-          
           </div>
         ) : null;
       });
     }
   };
-
   return (
     <>
       <div key="hey">{buildTemplate()}</div>
