d33709ac23b44f3c3549cf8f780be937e0782715
nicholas <brancifortnick@yahoo.com>
2024-07-20 07:30:07 -0400

    styling4.9.10000

--- react-app/src/components/AllImagesRefactor.js
+++ react-app/src/components/AllImagesRefactor.js
@@ -1,41 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Grid from "@mui/material/Grid";
+import {Typography } from "@mui/material";
 import DeleteLocationsImages from "./DeleteLocationsImages";
 import ImageList from "@mui/material/ImageList";
-
 import ImageListItem from "@mui/material/ImageListItem";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Card from "@mui/material/Card";
-import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
 import CardActions from "@mui/material/CardActions";
-import Typography from "@mui/material/Typography";
 import AddComments from "./AddComments";
-
-import {
-  getOneComment,
-  getTheComments,
-  updateAComment,
-} from "../store/comment";
 import Accordian from "./Accordian/Accordian";
-import Divider from "@mui/material/Divider";
 import DeleteComment from "./DeleteComment";
-import { findObjectById } from "../Utilities/StoreMethods";
 import EditComment from "./EditComment";
 import VoteCounter from "./VoteCounter";
 import ButtonGroup from "@mui/material/ButtonGroup";
-import { CardHeader } from "@mui/material";
-//mui styling
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Stack from "@mui/material/Stack";
-import { styled } from "@mui/material/styles";
-
+import ImageListItemBar from "@mui/material/ImageListItemBar";
 function AllImagesRefactor({ images }) {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.session.user);
   const location = useSelector((state) => state.location);
+
   const buildCommentTemplate = (image) => {
     if (image.comments && Array.isArray(image.comments)) {
       return image.comments.map((comment) => {
@@ -53,49 +37,44 @@ function AllImagesRefactor({ images }) {
   };
   //mui styling funcs for images
 
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  }));
-
   //returning multiple accordians
   const buildTemplate = () => {
     if (images) {
       return images.map((image) => {
         return image !== null && location.id === image.location_id ? (
-        
-            
-            <div className='images-container'> {image.title}
+          <div className="images-container">
+               <div>{image.title}</div>
            
+             
                 <img
                   src={image.image}
-                  alt="loading..."
                   className="image-card"
-                  />
-                
+                  alt="loading..."
+                />
+
+         
+               
+            
            
-              <section>
-                <span>
-                  <AddComments imageId={image.id} locationId={location.id} />
-                </span>
-                <span>
-                  <Accordian
-                    toggleText="comments"
-                    children={buildCommentTemplate(image)}
-                  />
-                </span>
-              </section>
-              <CardContent>
-                <CardActions>
-                  <DeleteLocationsImages imageId={image?.id} />
-                  {/* {user.id === Number(location.user_id) ? (  ) : null} */}
-                </CardActions>
-              </CardContent>
-       
-              </div>
+
+            <span>
+              <AddComments imageId={image.id} locationId={location.id} />
+            </span>
+            <span>
+              <Accordian
+                toggleText="comments"
+                children={buildCommentTemplate(image)}
+              />
+            </span>
+
+            <CardContent>
+              <CardActions>
+                <DeleteLocationsImages imageId={image?.id} />
+                {/* {user.id === Number(location.user_id) ? (  ) : null} */}
+              </CardActions>
+            </CardContent>
+          
+          </div>
         ) : null;
       });
     }
