adbbf5c5454818c1a7250a296db8dd02253dd494
nicholas <brancifortnick@yahoo.com>
2024-11-25 10:14:20 -0500

    got the data back for users profile page

--- react-app/src/components/UsersLocations.js
+++ react-app/src/components/UsersLocations.js
@@ -5,49 +5,35 @@ import { getAllLocations } from "../store/location";
 import './UsersLocations.css'
 import DeleteLocation from "./DeleteLocation";
 import UpdateBiography from "./UpdateBiography";
-import { useParams } from "react-router-dom";
 const UsersLocations = () => {
   const dispatch = useDispatch();
   const locations = useSelector((state) => Object.values(state.location));
   const user = useSelector((state) => state.session.user);
 
-  const { userId } = useParams();
-
   useEffect(() => {
-    dispatch(getAllLocations(userId));
-  }, [dispatch, userId]);
+    dispatch(getAllLocations());
+  }, [dispatch]);
 
   const usersLocations = locations.map((location, idx) => {
     return user.id === Number(location.user_id) ? (
       <div>
         <h4>Click on a location to view more details</h4>
         <div key={ idx } className="musician-ol">
-
+          {/* <div id="musician-name">{musician.musician_name}</div> */ }
           <NavLink
             to={ `/locations/${location.id}` }
           >{ `View ${location.location_name}` }</NavLink>
         </div>
         <div className="location-div">
-          <img src={ location.profile_img } alt="_blank" className="card" />
-          { user.id === Number(location.user_id) ? (
-            <div className="delete-location-profile-page">
-              <DeleteLocation locationId={ location.id } />
-            </div>
-          ) : null };
-          <div>
-            { user.id === Number(location.user_id) ? (
+          <img src={ location.profile_img } alt="_blank" className="card"></img>
 
-              <UpdateBiography locationId={ location.id } />
-
-            ) : null };
-
+          <div className="delete-location-profile-page">
+            <DeleteLocation locationId={ location.id } />
           </div>
-          <div>  { location.biography }</div>
-
 
         </div>
       </div>
-    ) : null
+    ) : null;
   });
 
   return (
