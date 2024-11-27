228a865b92bb5f28e22203daa9b4085deff20bbb
nicholas <brancifortnick@yahoo.com>
2024-11-26 20:57:02 -0500

    added footer icons---now style the footer asap and move on

--- react-app/src/components/Contact.js
+++ react-app/src/components/Contact.js
@@ -5,10 +5,11 @@ const Contact = () => {
   return (
     <div className="contact-page-wrapper">
       <h1 className="primary-heading">Have Question In Mind?</h1>
-      <h1 className="primary-heading">Let Us Help You</h1>
+      <h1 className="primary-heading">Let Us Help You: Email Us Here ▼</h1>
       <div className="contact-form-container">
-        <input type="text" placeholder="" />
-        <button className="secondary-button" >Submit</button>
+        <input type="text" placeholder=" brancifortnick@gmail.com" />
+        <a href='https://docs.google.com/document/d/14LgaPDgC3hyTzoPiOpuWaH9XQ4j-54wVJJQsTvknoYE/edit?usp=sharing'></a>
+
       </div>
     </div>
   );
