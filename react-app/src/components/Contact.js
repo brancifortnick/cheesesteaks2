c5f4e4688111c3c6038c2d66ab9f8575963e7eb0
nicholas <brancifortnick@yahoo.com>
2024-11-26 21:49:57 -0500

    fixed footer icons and styled it properly---finally

--- react-app/src/components/Contact.js
+++ react-app/src/components/Contact.js
@@ -4,11 +4,13 @@ import './Contact.css'
 const Contact = () => {
   return (
     <div className="contact-page-wrapper">
-      <h1 className="primary-heading">Have Question In Mind?</h1>
+
       <h1 className="primary-heading">Let Us Help You: Email Us Here ▼</h1>
       <div className="contact-form-container">
-        <input type="text" placeholder=" brancifortnick@gmail.com" />
-        <a href='https://docs.google.com/document/d/14LgaPDgC3hyTzoPiOpuWaH9XQ4j-54wVJJQsTvknoYE/edit?usp=sharing'></a>
+        <h1 className='show-email'>
+          brancifortnick@gmail.com
+        </h1>
+
 
       </div>
     </div>
