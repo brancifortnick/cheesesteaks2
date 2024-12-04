774ffa929ae008275a071482aa44e73dbeee0f9c
nicholas <brancifortnick@yahoo.com>
2024-12-04 09:46:38 -0500

    fixing locations/id page aka details page

--- react-app/src/components/LocationDetails.js
+++ react-app/src/components/LocationDetails.js
@@ -1,24 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { getOneLocation } from "../store/location";
-import { getPhotos } from "../store/image";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import UpdateBiography from "./UpdateBiography";
-import ImageUpload from "./ImageUpload";
-import AllImagesRefactorTwo from "./AllImagesRefactorTwo";
-import "./LocationDetails.css";
-import CardActions from "@mui/material/CardActions";
-import { CardContent, CardHeader } from "@mui/material";
-import { MapContainer } from "./MapContainer";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import AllImagesRefactorThree from "./AllImagesRefactor3";
-import Avatar from "@mui/material/Avatar";
-import Footer from './Footer'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { getOneLocation } from '../store/location';
+import { getPhotos } from '../store/image';
+import UpdateBiography from './UpdateBiography';
+import ImageUpload from './ImageUpload';
+import AllImagesRefactorThree from './AllImagesRefactor3';
+import './LocationDetails.css'; // Import the CSS file
 
 function LocationDetails() {
   const dispatch = useDispatch();
@@ -36,41 +25,32 @@ function LocationDetails() {
   return (
     <>
       <div className="location-details-container">
-
         <div className="location-details-image-container">
           <h1 className='location-name'>{ location.location_name }</h1>
-          <Box  sx={{display: 'flex',flexDirection: 'row', justifyContent: 'center'}}>
+          <Box sx={ { display: 'flex', flexDirection: 'row', justifyContent: 'center' } }>
             <img style={ { borderRadius: '20px' } } src={ location.profile_img } alt="location" className="location-details-image" />
           </Box>
           <div className='location-name'>
-
             { location.address + ',' + " " }
             { location.city + ',' + " " }
             { location.state + ',' + " " }
             { location.zipcode }
-
           </div>
           { currentUser.id === location.user_id ? (
             <div className='biography-container'>
-
               <UpdateBiography locationId={ locationId } />
-
-
-            </div>) : null }
-          <div className="biography-wrapper"> { location.biography }</div>
+            </div>
+          ) : null }
+          <div className="biography-wrapper">{ location.biography }</div>
           <div className='upload-image-button'>
             <ImageUpload locationId={ locationId } />
           </div>
-
           <div className="images-inner-container">
-          <AllImagesRefactorThree images={ images } locationId={ locationId } />
+            <AllImagesRefactorThree images={ images } locationId={ locationId } />
+          </div>
         </div>
-        </div>
-
       </div>
-
     </>
-
   );
 }
 
